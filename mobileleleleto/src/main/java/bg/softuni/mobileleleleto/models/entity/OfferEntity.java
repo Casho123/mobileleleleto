44a81dede56dcdec47448da7ed0a43f331ebebd7
My PC <alex.petkov606@gmail.com>
2023-03-28 21:18:15 +0300

    asd

--- mobileleleleto/src/main/java/bg/softuni/mobileleleleto/models/entity/OfferEntity.java
+++ mobileleleleto/src/main/java/bg/softuni/mobileleleleto/models/entity/OfferEntity.java
@@ -1,12 +1,12 @@
 package bg.softuni.mobileleleleto.models.entity;
 
+import bg.softuni.mobileleleleto.models.entity.enums.EngineEnum;
+import bg.softuni.mobileleleleto.models.entity.enums.TransmissionEnum;
 import org.hibernate.annotations.GenericGenerator;
 import org.hibernate.annotations.Type;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.math.BigDecimal;
 import java.util.UUID;
 
 @Entity
@@ -14,8 +14,30 @@ import java.util.UUID;
 public class OfferEntity {
     @Id
     @GeneratedValue(generator = "UUID")
-    @GenericGenerator(name = "UUID", strategy = "org.hibernate.UUIDGenerator")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
     @Type(type = "uuid-char")
     private UUID id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private EngineEnum engine;
+
+    @Column(name = "image_url")
+    private String imageUrl;
+    private int mileage;
+    private BigDecimal price;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private TransmissionEnum transmission;
+
+    private int year;
+
+    @ManyToOne
+    private ModelEntity model;
+
+    @ManyToOne
+    private UserEntity user;
+
 
 }
---
+++ mobileleleleto/src/main/java/bg/softuni/mobileleleleto/models/entity/enums/TransmissionEnum.java
@@ -0,0 +1,7 @@
+package bg.softuni.mobileleleleto.models.entity.enums;
+
+public enum TransmissionEnum {
+
+    MANUAL,
+    AUTOMATIC
+}

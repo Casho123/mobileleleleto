2cd01719308d7fa49073c6750b45496e0dc126bd
My PC <alex.petkov606@gmail.com>
2023-03-28 21:00:42 +0300

    asd

--- mobileleleleto/src/main/java/bg/softuni/mobileleleleto/models/entity/UserEntity.java
+++ mobileleleleto/src/main/java/bg/softuni/mobileleleleto/models/entity/UserEntity.java
@@ -1,17 +1,93 @@
 package bg.softuni.mobileleleleto.models.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "users")
 public class UserEntity extends BaseEntity {
 
-    private String username;
+    @Column(nullable = false)
+    private String email;
+    @Column
     private String password;
+    @Column(name = "first_name")
     private String firstName;
+    @Column(name = "last_name")
     private String lastName;
+    @Column(name = "is_active")
     private boolean isActive;
+    @Column(name = "image_url")
     private String imageUrl;
 
+    @ManyToMany
+    private List<UserRoleEntity> userRoles = new ArrayList<>();
+
+    public UserEntity() {
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean active) {
+        isActive = active;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public List<UserRoleEntity> getUserRoles() {
+        return userRoles;
+    }
+
+    public void setUserRoles(List<UserRoleEntity> userRoles) {
+        this.userRoles = userRoles;
+    }
+
+    public void addRole(UserRoleEntity userRole) {
+        this.userRoles.add(userRole);
+
+    }
 }

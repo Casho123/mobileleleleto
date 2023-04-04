a4dbcbf69f911fc95e3a5cb461ad20f1fcc5f5a9
My PC <alex.petkov606@gmail.com>
2023-04-04 22:16:02 +0300

    asd

--- mobileleleleto/src/main/java/bg/softuni/mobileleleleto/models/dto/UserRegistrationDTO.java
+++ mobileleleleto/src/main/java/bg/softuni/mobileleleleto/models/dto/UserRegistrationDTO.java
@@ -1,10 +1,22 @@
 package bg.softuni.mobileleleleto.models.dto;
 
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
 public class UserRegistrationDTO {
 
+    @Email
     private String email;
+    @NotEmpty
+    @Size(min=2, max=20)
     private String firstName;
+    @NotEmpty
+    @Size(min=2, max=20)
     private String lastName;
+    @NotEmpty
+    @Size(min=5)
     private String password;
     private String confirmPassword;
 
--- mobileleleleto/src/main/java/bg/softuni/mobileleleleto/web/UserRegistrationController.java
+++ mobileleleleto/src/main/java/bg/softuni/mobileleleleto/web/UserRegistrationController.java
@@ -1,6 +1,5 @@
 package bg.softuni.mobileleleleto.web;
 
-import bg.softuni.mobileleleleto.models.dto.UserLoginDTO;
 import bg.softuni.mobileleleleto.models.dto.UserRegistrationDTO;
 import bg.softuni.mobileleleleto.service.UserService;
 import org.springframework.stereotype.Controller;
@@ -11,33 +10,29 @@ import org.springframework.web.bind.annotation.PostMapping;
 import javax.validation.Valid;
 
 @Controller
-public class UserController {
+public class UserRegistrationController {
 
     private final UserService userService;
 
-    public UserController(UserService userService) {
+    public UserRegistrationController(UserService userService) {
         this.userService = userService;
     }
 
-    @GetMapping("/users/login")
-    public String login() {
-        return "auth-login";
+
+    @GetMapping("/users/register")
+    public String register() {
+        return "auth-register";
     }
 
-    @GetMapping("/users/logout")
-    public String logout() {
-        this.userService.logout();
+    @PostMapping("/users/register")
+    public String register(@Valid UserRegistrationDTO userRegistrationDTO,
+                           BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "redirect:/users/register";
+        }
+
+        this.userService.registerAndLogin(userRegistrationDTO);
         return "redirect:/";
-    }
-
-    @PostMapping("/users/login")
-    public String login(UserLoginDTO userLoginDTO) {
-        System.out.println("User is logged: " + userService.login(userLoginDTO));
-
-        return "redirect:/";
 
     }
-
-
-
 }

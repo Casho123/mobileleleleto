3a0e807fa0c49ce65e68c180802958b8c06c9e0a
My PC <alex.petkov606@gmail.com>
2023-04-17 13:47:59 +0300

    asd

--- mobileleleleto/src/main/java/bg/softuni/mobileleleleto/models/user/MobileleUserDetails.java
+++ mobileleleleto/src/main/java/bg/softuni/mobileleleleto/models/user/MobileleUserDetails.java
@@ -6,6 +6,24 @@ import org.springframework.security.core.userdetails.UserDetails;
 import java.util.Collection;
 
 public class MobileleUserDetails implements UserDetails {
+
+    private String password;
+    private String username;
+    private String firstName;
+    private String lastName;
+    private Collection<GrantedAuthority> authorities;
+
+    public MobileleUserDetails(String password, String username,
+                               String firstName, String lastName,
+                               Collection<GrantedAuthority> authorities) {
+
+        this.password = password;
+        this.username = username;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.authorities = authorities;
+    }
+
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return null;
@@ -23,21 +41,21 @@ public class MobileleUserDetails implements UserDetails {
 
     @Override
     public boolean isAccountNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isEnabled() {
-        return false;
+        return true;
     }
 }

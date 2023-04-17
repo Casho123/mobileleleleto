b157f14a82f8593fff1de6bd9c8e58c39a6ac722
My PC <alex.petkov606@gmail.com>
2023-04-17 13:55:15 +0300

    asd

--- mobileleleleto/src/main/java/bg/softuni/mobileleleleto/service/MobileleleUserDetailsService.java
+++ mobileleleleto/src/main/java/bg/softuni/mobileleleleto/service/MobileleleUserDetailsService.java
@@ -1,14 +1,48 @@
 package bg.softuni.mobileleleleto.service;
 
 
+import bg.softuni.mobileleleleto.models.entity.UserEntity;
+import bg.softuni.mobileleleleto.models.entity.UserRoleEntity;
+import bg.softuni.mobileleleleto.models.user.MobileleUserDetails;
+import bg.softuni.mobileleleleto.repository.UserRepository;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 public class MobileleleUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public MobileleleUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     @Override
     public UserDetails loadUserByUsername(String username)
             throws UsernameNotFoundException {
-        return null;
+        return this.userRepository.findByEmail(username)
+                .map(this::map)
+                .orElseThrow(() -> new UsernameNotFoundException("User with " + username + " not found!"));
+    }
+
+    private UserDetails map(UserEntity userEntity) {
+
+        return new MobileleUserDetails(
+                userEntity.getPassword(),
+                userEntity.getEmail(),
+                userEntity.getFirstName(),
+                userEntity.getLastName(),
+                userEntity.getUserRoles()
+                        .stream().map(this::map)
+                        .toList()
+        );
+    }
+
+
+    private GrantedAuthority map(UserRoleEntity userRole) {
+        return new SimpleGrantedAuthority("ROLE_" +
+                userRole.getUserRole().name());
     }
 }

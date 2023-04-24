7a644bf96c3510eb19d7b0a4c9b52dd6aec469df
My PC <alex.petkov606@gmail.com>
2023-04-24 16:23:59 +0300

    asd

--- mobileleleleto/src/main/java/bg/softuni/mobileleleleto/config/MailConfig.java
+++ mobileleleleto/src/main/java/bg/softuni/mobileleleleto/config/MailConfig.java
@@ -1,25 +1,39 @@
 package bg.softuni.mobileleleleto.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
 
 @Configuration
 public class MailConfig {
 
 
     @Bean
-    public JavaMailSender javaMailSender() {
+    public JavaMailSender javaMailSender(
+            @Value("${mail.host}") String mailHost,
+            @Value("${mail.port}") Integer mailPort,
+            @Value("${mail.username}") String userName,
+            @Value("${mail.password}") String password
+    ) {
 
         JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
-        javaMailSender.setHost();
-        javaMailSender.setPort();
-        javaMailSender.setUsername();
-        javaMailSender.setPassword();
-        javaMailSender.setJavaMailProperties();
+        javaMailSender.setHost(mailHost);
+        javaMailSender.setPort(mailPort);
+        javaMailSender.setUsername(userName);
+        javaMailSender.setPassword(password);
+        javaMailSender.setJavaMailProperties(mailProperties());
 
         return javaMailSender;
+    }
 
+    private Properties mailProperties() {
+        Properties properties = new Properties();
+        properties.setProperty("mail.smtp.auth", "true");
+        properties.setProperty("mail.transport.protocol", "smtp");
+        return properties;
     }
 }
